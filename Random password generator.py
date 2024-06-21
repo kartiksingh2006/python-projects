3436ac1bc763470c7765d33fdafd4c57d69251ed
kartik singh <kartik.singh.dav@gmail.com>
2024-06-21 16:10:38 +0530

    Update Random password generator.py

--- Random password generator.py
+++ Random password generator.py
@@ -1,12 +1,13 @@
+#we will first write a greeting message 
 a= ("welcome to our random password generetor service!! ")
 print(a.upper())
-
+#then import random and string
 import random
 
 import string
-
+#define paswrod length
 pass_len=12
-
+#then add different cases , string , number and symbols.
 charactervalues = string.ascii_uppercase + string.ascii_lowercase + string.punctuation
 # print(charactervalues)
 
