29a4aa03490941d09cbb4e1dadae3f2294ca163b
kartik singh <kartik.singh.dav@gmail.com>
2024-06-21 11:27:18 +0530

    Update Increasing sequence stars.py

--- Increasing sequence stars.py
+++ Increasing sequence stars.py
@@ -1,4 +1,7 @@
+#first we will take input from user thet how many times he/she want to print the pattern of star.
 num= int(input("enter your digit: "))
+#now we will define a range 
+#you can also edit the range as per your requirement!
 for i in range(1,20):
     print(num, "*" ,i, "=" , num*i)
 
